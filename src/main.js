9386925c14e5978d75ecced610df6fc53aa41a4f
your name <you@example.com>
2019-09-23 22:48:25 +0300

    from video

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from "./router";
-import VueYandexMetrika from 'vue-yandex-metrika'  
+// import VueYandexMetrika from 'vue-yandex-metrika'  
 
 // import "bootstrap/dist/css/bootstrap.css";
 import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
-Vue.use(VueYandexMetrika, {
-  id: 55456729,
-  env: process.env.NODE_ENV
-  // other options
-})
+// Vue.use(VueYandexMetrika, {
+//   id: 55456729,
+//   env: process.env.NODE_ENV
+//   // other options
+// })
 
 new Vue({
   router,
