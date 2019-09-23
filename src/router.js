f2cfedb779f086330cbf692d9069988a152b566a
your name <you@example.com>
2019-09-24 00:19:24 +0300

    decodeURIComponent14

--- src/router.js
+++ src/router.js
@@ -5,6 +5,11 @@ import StatMain from "./views/StatMain.vue";
 
 Vue.use(Router);
 
+console.log(route);
+console.log(route.query);
+console.log(route.query.country);
+console.log(route.query.division);
+
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
