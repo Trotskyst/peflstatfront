ce768d13f313997e551da542feaccf73d16b2459
your name <you@example.com>
2019-09-24 10:24:55 +0300

    test stat route9

--- server.js
+++ server.js
@@ -14,29 +14,26 @@
 // console.log('Listen on port: ' + port)
 
 
-const express = require('express');
-const app = express();
-const serveStatic = require('serve-static')
-const path = require('path');
-const port = process.env.PORT || 8000;
-const server = require('http').Server(app);
-
-// app.use(express.static(__dirname, 'dist', {index: false}));
-// app.use(express.static(__dirname + '/'));
-// app.use(express.static('/'));
-app.use('/', serveStatic(path.join(__dirname, '/')))
+var express = require('express')
+// var history = require('connect-history-api-fallback')
+var path = require('path')
+var serveStatic = require('serve-static')
 
 
-server.listen(port, function() {
-    console.log("App running on port " + port);
+var app = express()
+
+// Use a fallback for non-root routes (required for Vue router)
+//   NOTE: History fallback must be "used" before the static serving middleware!
+// app.use(history({
+    // OPTIONAL: Includes more verbose logging
+    // verbose: true
+// }))
+
+// Serve static assets from the build files (images, etc)
+app.use(serveStatic(path.join(__dirname, '/dist')))
+
+var port = process.env.PORT || 8080
+
+app.listen(port, () => {
+  console.log('Server started at http://localhost:5000')
 })
-
-// PathLocationStrategy
-
-app.get('', function(req, res) {
-    res.sendFile(path.join(__dirname, 'src', 'index.html'));
-});
-
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'src', 'index.html'));
-});
