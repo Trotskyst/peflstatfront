af02a5092b7922615ec6e93d553fac71368e2d3d
your name <you@example.com>
2019-09-24 10:44:27 +0300

    test stat route10

--- server.js
+++ server.js
@@ -14,26 +14,9 @@
 // console.log('Listen on port: ' + port)
 
 
-var express = require('express')
-// var history = require('connect-history-api-fallback')
-var path = require('path')
-var serveStatic = require('serve-static')
+const express = require('express');
+const app = express();
 
+app.use(express.static(__dirname + '/dist'));
 
-var app = express()
-
-// Use a fallback for non-root routes (required for Vue router)
-//   NOTE: History fallback must be "used" before the static serving middleware!
-// app.use(history({
-    // OPTIONAL: Includes more verbose logging
-    // verbose: true
-// }))
-
-// Serve static assets from the build files (images, etc)
-app.use(serveStatic(path.join(__dirname, '/dist')))
-
-var port = process.env.PORT || 8080
-
-app.listen(port, () => {
-  console.log('Server started at http://localhost:5000')
-})
+const port = process.env.PORT || 5000;
