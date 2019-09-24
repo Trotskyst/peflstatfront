53b0b6a92267c0f9309adbe296c7f963fb87d610
your name <you@example.com>
2019-09-24 09:14:14 +0300

    test stat route2

--- server.js
+++ server.js
@@ -6,7 +6,24 @@ const app = express()
 
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
+
+
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+  
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+
+
+
 const port = process.env.PORT || 8080
 app.listen(port)
+
+
 
 console.log('Listen on port: ' + port)
