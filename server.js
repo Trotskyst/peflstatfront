2a42419056e5da37e1521e6db5848b008ef1b0cc
your name <you@example.com>
2019-09-24 10:50:34 +0300

    test stat route11

--- server.js
+++ server.js
@@ -14,9 +14,15 @@
 // console.log('Listen on port: ' + port)
 
 
-const express = require('express');
-const app = express();
+var history = require('connect-history-api-fallback');
 
-app.use(express.static(__dirname + '/dist'));
-
-const port = process.env.PORT || 5000;
+var express = require('express')
+var path = require('path')
+var serveStatic = require('serve-static')
+app = express()
+//add this middleware
+app.use(history());    
+app.use(serveStatic(__dirname))
+var port = process.env.PORT || 5000
+app.listen(port)
+console.log('server started '+ port) 
